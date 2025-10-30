01c6b71356480b88e7736b0ab08836a3e544e3eb
datnguyendaoqn <daonguyendat01637@gmail.com>
2025-10-30 15:45:12 +0700

    feat(dto): add new response fields to LoginResponse DTO

--- BackendEcommerce/Application/Auth/DTOs/LoginResponseDTO.cs
+++ BackendEcommerce/Application/Auth/DTOs/LoginResponseDTO.cs
@@ -2,6 +2,9 @@
 {
     public class LoginResponseDTO
     {
+        public string FullName { get; set; } = null!;
+        public string Role { get; set; } = null!;
+        public string ID { get; set; } = null!;
         public string AccessToken { get; set; } = null!;
         public string RefreshToken { get; set; } = null!;
 
